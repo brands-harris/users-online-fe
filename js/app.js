d5ab73796d4207d915d429a459ef77394caa785d
brandon@fusebox.co.za <brandon@fusebox.co.za>
2023-03-14 21:53:41 +0200

    WIP: users online

--- js/app.js
+++ js/app.js
@@ -5,24 +5,32 @@ const fetchUsersOnline = async () => {
         .then((r) => r.json())
         .catch((err) => onErr(err));
     if (resp.success) {
-        const el = await fetch(`html/fragments/error.html`).then((r) => r.text());
-        el.replace('{msg}', resp.message);
-        document.getElementById('content').innerHTML = el;
+        onSuccess(resp.data);
     } else {
         onErr(resp.msg || 'An error has occurred');
     }
 };
-const renderUserEl = (data) => {
-    //
-};
 
-const interval = setInterval(fetchUsersOnline, 3000);
+const onSuccess = async (data) => {
+    let layoutEl = await fetch(`/html/fragments/users.html`).then((r) => r.text());
+    layoutEl = layoutEl.replace(/{usersOnline}/g, data.length);
+    const userEl = [];
+    data.forEach((item) => {
+        //TODO: finish element
+        const str = `<div class='g-col-4 d-flex flex-column'><p>Name : ${item.name}</p></div>`;
+        userEl.push(str);
+    });
+    layoutEl = layoutEl.replace(/{content}/g, userEl.join(''));
+    document.getElementById('content').innerHTML = layoutEl;
+};
 
 const onErr = async (msg) => {
     clearInterval(interval);
-    const el = await fetch(`/html/fragments/error.html`).then((r) => r.text());
+    let el = await fetch(`/html/fragments/error.html`).then((r) => r.text());
     el.replace('{msg}', msg);
     document.getElementById('content').innerHTML = el;
 };
+
+const interval = setInterval(fetchUsersOnline, 3000);
 
 fetchUsersOnline();
